bf1e139092e37adccec1431c37a10ef177910235
Abigiya <ajorken1502@gmail.com>
2025-07-30 19:17:07 +0300

    update

--- src/main/java/util/FileLogger.java
+++ src/main/java/util/FileLogger.java
@@ -5,5 +5,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FileLogger {
-    
+    private static final String LOG_FILE = "log.txt";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 }
