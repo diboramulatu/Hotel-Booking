3f31330c326dfb95330face9514ede1bdbee5ff5
Abigiya <ajorken1502@gmail.com>
2025-07-30 19:10:49 +0300

    update

--- src/main/java/interfaces/ReportGenerator.java
+++ src/main/java/interfaces/ReportGenerator.java
@@ -6,5 +6,5 @@ import java.util.List;
 public interface ReportGenerator {
     String generateBookingReport(List<Booking> bookings);
     String generateRoomOccupancyReport();
-    
+    String generateRevenueReport();
 }
