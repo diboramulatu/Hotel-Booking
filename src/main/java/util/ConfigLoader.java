06d71a8184bdc286aa561ba040f819422c51181f
Abigiya <ajorken1502@gmail.com>
2025-07-30 19:12:40 +0300

    update

--- src/main/java/util/ConfigLoader.java
+++ src/main/java/util/ConfigLoader.java
@@ -4,5 +4,12 @@ import java.io.*;
 import java.util.Properties;
 
 public class ConfigLoader {
+    private static final String CONFIG_FILE = "src/main/resources/config.txt";
+    private static Properties properties;
     
+    static {
+        loadConfig();
+    }
+    
+
 }
