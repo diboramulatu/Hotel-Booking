78cbfc916d1b9df6641b61c91f2adb80ee49e10c
Abigiya <ajorken1502@gmail.com>
2025-07-30 21:08:25 +0300

    update

--- src/main/java/util/FileLogger.java
+++ src/main/java/util/FileLogger.java
@@ -9,6 +9,9 @@ public class FileLogger {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     
     public static void log(String level, String message) {
+        String timestamp = LocalDateTime.now().format(formatter);
+        String logEntry = String.format("[%s] %s: %s%n", timestamp, level, message);
+        
         
     }
 }
